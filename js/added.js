340f8052979ff11f8efb69cea5f9eb45a8e493c0
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-15 17:09:11 +0100

    Update domain for signIn function

--- js/added.js
+++ js/added.js
@@ -1,5 +1,5 @@
 function signIn() {
-  const domain = "https://us-west-1mwmvk9jtt.auth.us-west-1.amazoncognito.com";
+  const domain = "us-west-1mwmvk9jtt";
   const clientId = "2nvt55hgmp4dr3305drocl11dt";
   const redirectUri = "https://main.d3enyvo3amjsbz.amplifyapp.com/";
 
