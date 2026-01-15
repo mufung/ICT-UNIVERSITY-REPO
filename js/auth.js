b8cd61dad813278bb59e080811574e650afa7f3e
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-15 18:08:47 +0100

    Implement Cognito sign-in functionality
    Added Cognito login URL construction in signIn function.

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,15 @@
 function signIn() {
-    alert("Cognito sign-in will be connected here");
-}
+  const domain = "us-west-1mmwvk9jtt";
+  const region = "us-west-1";
+  const clientId = "2nvt5hgnp4dr3305droc11dt";
+  const redirectUri = "https://main.d3enyvo3amjsbz.amplifyapp.com/index.html";
 
-function register() {
-    alert("Cognito registration will be connected here");
+  const loginUrl =
+    `https://${domain}.auth.${region}.amazoncognito.com/login` +
+    `?client_id=${clientId}` +
+    `&response_type=token` +
+    `&scope=openid+email+profile` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  window.location.href = loginUrl;
 }
