d145aa4441be979240114e7c26618c6756da4f5d
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-15 16:25:16 +0100

    Update Cognito user pool IDs in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const _config = {
     cognito: {
-        userPoolId: 'us-west-1_mWMVK9JTt',
-        userPoolClientId: '53mnaeufjru4tpvvh3lq74dkce',
+        userPoolId: 'us-west-1_ZcPGRDAB1',
+        userPoolClientId: '3537i7gssuj7dotmphqdkbu434',
         region: 'us-west-1'
     },
     api: {
