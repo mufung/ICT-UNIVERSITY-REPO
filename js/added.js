aa18a3effd68d78dfadf9fc558b1e1786d7ba379
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-15 16:49:31 +0100

    Update Cognito domain and client ID in signIn function

--- js/added.js
+++ js/added.js
@@ -1,6 +1,6 @@
 function signIn() {
-  const domain = "https://us-west-1zcpgrdab1.auth.us-west-1.amazoncognito.com";
-  const clientId = "3537i7gssuj7dotmphqdkbu434";
+  const domain = "https://us-west-1mwmvk9jtt.auth.us-west-1.amazoncognito.com";
+  const clientId = "2nvt55hgmp4dr3305drocl11dt";
   const redirectUri = "https://main.d3enyvo3amjsbz.amplifyapp.com/";
 
   const loginUrl =
