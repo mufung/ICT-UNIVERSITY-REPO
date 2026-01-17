aa6f6730689a7d7a538f3b55ca57f5235f298db5
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-17 13:07:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,18 @@
-const COGNITO_CONFIG = {
-  domain: "https://us-west-1mbnfq4dcv.auth.us-west-1.amazoncognito.com",
-  clientId: "7ugvqkg6l7859oto4v5vcjqjus",
-  redirectUri: "http://localhost:5500/auth-callback.html",
-  scopes: "openid email profile",
-  responseType: "token"
+// ================================
+// GLOBAL CONFIGURATION
+// ================================
+
+// 🔴 IMPORTANT: This is your API Gateway Invoke URL
+// MUST end with the stage name (e.g. /dev)
+const API_BASE_URL = "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev";
+
+// API endpoints
+const API_ENDPOINTS = {
+    teacherSubmitResult: `${API_BASE_URL}`,
+};
+
+// Export for all JS files
+window.APP_CONFIG = {
+    API_BASE_URL,
+    API_ENDPOINTS
 };
