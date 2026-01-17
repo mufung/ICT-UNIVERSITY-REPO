506eb55c2a8145d47963c9f2619073bec491a59a
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-17 17:47:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-const API_BASE_URL = "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev";
-
-window.APP_CONFIG = {
-  SUBMIT_RESULT_URL: `${API_BASE_URL}/submit-result`
+ const config = {
+  API_BASE_URL: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
 };
