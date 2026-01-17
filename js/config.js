ef2f02f82132bd7f77650d7a6750a4caaa647b6b
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-17 15:15:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,5 @@
-// ================================
-// GLOBAL CONFIGURATION
-// ================================
-
-// IMPORTANT: MUST point to /submit-result
 const API_BASE_URL = "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev";
 
-// API endpoints
-const API_ENDPOINTS = {
-  teacherSubmitResult: `${API_BASE_URL}/submit-result`
-};
-
-// Export globally
 window.APP_CONFIG = {
-  API_BASE_URL,
-  API_ENDPOINTS
+  SUBMIT_RESULT_URL: `${API_BASE_URL}/submit-result`
 };
