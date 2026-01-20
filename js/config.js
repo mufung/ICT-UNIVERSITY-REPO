b74a6e41ce0be829f16bffc8f54128ee729c4b03
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-20 01:26:54 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-const CONFIG = {
-  TEACHER_API_URL: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev/submit-result",
-  STUDENT_API_URL: "https://<YOUR-STUDENT-API-ID>.execute-api.us-west-1.amazonaws.com/get-results"
+ const CONFIG = {
+  TEACHER_API_URL: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev/submit-result"
 };
