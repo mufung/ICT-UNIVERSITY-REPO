19ae6872c77a3c85d77fddede99771c3e42f1c76
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-20 13:23:30 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,18 @@
-window.config = {
-  API_BASE_URL: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
+ window.config = {
+  // HTTP API INVOKE URL (base)
+  API_BASE_URL: "https://7qpx0p1fwg.execute-api.us-west-1.amazonaws.com",
+
+  // Stage
+  API_STAGE: "dev",
+
+  // FULL BASE (computed, safer)
+  get API_INVOKE_URL() {
+    return `${this.API_BASE_URL}/${this.API_STAGE}`;
+  },
+
+  // Endpoints
+  ENDPOINTS: {
+    TEACHER_RESULTS: "/teacher-results",
+    STUDENT_RESULTS: "/student-results"
+  }
 };
