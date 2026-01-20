fcaa513238f46e071e96b470cae0b33c1efd325b
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-20 13:30:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,16 @@
  window.config = {
-  API_BASE_URL: "https://7qpx0p1fwg.execute-api.us-west-1.amazonaws.com",
+  // === API CORE ===
+  API_BASE_URL: "https://dx2imd746.execute-api.us-west-1.amazonaws.com",
   API_STAGE: "prod",
 
+  // === AUTO-BUILT INVOKE URL ===
   get API_INVOKE_URL() {
     return `${this.API_BASE_URL}/${this.API_STAGE}`;
   },
 
+  // === ENDPOINTS (DO NOT BREAK TEACHER) ===
   ENDPOINTS: {
-    TEACHER_RESULTS: "/teacher-results",
+    TEACHER_SUBMIT_RESULT: "/submit-result",
     STUDENT_RESULTS: "/student-results"
   }
 };
