cc980f54a353aab495ee57a95bbcf953bf33e05b
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-22 22:03:11 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@
   cognito: {
     region: "us-west-1",
     userPoolId: "us-west-1_mbnfq4DcV",
-    clientId: "7ugvqk6l7859oto4v5vcjqjus" 
+    clientId: "7ugvqkg6l7859oto4v5vcjqjus" 
   },
   api: {
     baseUrl: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
   }
 };
-export default config;
+// Make it available globally for the non-module scripts
+window.appConfig = config;
