c30c776b6da77ea0b0db13d912e0be739b470749
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-22 10:51:24 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
- window.config = {
-  // ===== BASE (DO NOT BREAK – teacher already depends on this) =====
-  API_BASE_URL: "https://7qpx0p1fwg.execute-api.us-west-1.amazonaws.com/dev",
-
-  // ===== TEACHER API (REST API) =====
-  TEACHER_RESULTS_ENDPOINT: "/results",
-
-  // ===== STUDENT API (HTTP API – prod stage) =====
-  STUDENT_RESULTS_API: "https://dxc2imd746.execute-api.us-west-1.amazonaws.com/prod/student-results"
+// config.js
+const config = {
+  api: {
+    baseUrl: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
+  }
 };
+
+export default config;
