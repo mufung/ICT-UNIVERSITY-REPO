7cbdadde94594ec618727083fe6c6066a98cd81e
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-22 21:40:31 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
   cognito: {
     region: "us-west-1",
     userPoolId: "us-west-1_mbnfq4DcV",
-    clientId: "7ugvqkg6l7859oto4v5vcjqjus" 
+    clientId: "7ugvqk6l7859oto4v5vcjqjus" 
   },
   api: {
     baseUrl: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
