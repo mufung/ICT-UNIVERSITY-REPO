ec012a45d501d7e8cda04a9a143206901047eac4
Mufung Angelbell Mbuyeh <mufungangelbell@gmail.com>
2026-01-22 20:46:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,12 +2,10 @@
   cognito: {
     region: "us-west-1",
     userPoolId: "us-west-1_mbnfq4DcV",
-    clientId: "7ugvqk6l7859oto4v5vcjqjus" // Verified Client ID
+    clientId: "7ugvqkg6l7859oto4v5vcjqjus" 
   },
   api: {
     baseUrl: "https://7qpx0plfwg.execute-api.us-west-1.amazonaws.com/dev"
-  },
-  tableName: "teacher-results-table"
+  }
 };
-
 export default config;
